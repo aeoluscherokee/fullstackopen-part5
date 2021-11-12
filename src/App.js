e3f997ac19a1248c13617f7f7ec97b191e5bf14c
Krittanu Royapan <krittanu.riyapan@gmail.com>
2021-11-12 18:03:08 +0700

    Add login functionality

--- src/App.js
+++ src/App.js
@@ -1,24 +1,68 @@
-import React, { useState, useEffect } from 'react'
-import Blog from './components/Blog'
-import blogService from './services/blogs'
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+import Blog from './components/Blog';
+import blogService from './services/blogs';
 
 const App = () => {
-  const [blogs, setBlogs] = useState([])
+  const [blogs, setBlogs] = useState([]);
+  const [loginData, setLoginData] = useState({ username: '', password: '' });
+  const [userData, setUserData] = useState({
+    token: '',
+    username: '',
+    name: '',
+  });
 
   useEffect(() => {
-    blogService.getAll().then(blogs =>
-      setBlogs( blogs )
-    )  
-  }, [])
+    blogService.getAll().then((blogs) => setBlogs(blogs));
+  }, []);
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    const response = await axios.post('/api/users/login', loginData);
+    if (response.status === 200) {
+      setUserData(response.data);
+    }
+  };
+  const handleOnChange = (e) => {
+    setLoginData((prev) => {
+      return { ...prev, [e.target.name]: e.target.value };
+    });
+  };
+  const handleLogout = (e) => {
+    setUserData({});
+  };
 
   return (
     <div>
-      <h2>blogs</h2>
-      {blogs.map(blog =>
-        <Blog key={blog.id} blog={blog} />
+      {userData.username ? (
+        <div>
+          <h2>blogs</h2>
+          <p>
+            {userData.name} logged in{' '}
+            <button onClick={handleLogout}>logout</button>
+          </p>
+          {blogs.map((blog) => (
+            <Blog key={blog.id} blog={blog} />
+          ))}
+        </div>
+      ) : (
+        <div>
+          <h1>log in to application</h1>
+          <form onSubmit={handleSubmit}>
+            username
+            <input name="username" onChange={handleOnChange}></input>
+            password
+            <input
+              type="password"
+              name="password"
+              onChange={handleOnChange}
+            ></input>
+            <button type="submit">submit</button>
+          </form>
+        </div>
       )}
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
