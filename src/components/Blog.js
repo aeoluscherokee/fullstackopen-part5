376be14b9ccb9fc83cbca7e2fb0078334bda0eb6
Krittanu Royapan <krittanu.riyapan@gmail.com>
2021-11-15 17:05:51 +0700

    Add button to show and hide blog details

--- src/components/Blog.js
+++ src/components/Blog.js
@@ -1,7 +1,15 @@
 import React from 'react';
+import Togglable from './Togglable';
 const Blog = ({ blog }) => (
   <div>
     {blog.title} {blog.author}
+    <Togglable showLabel="view" hideLabel="hide">
+      <div>
+        <p>{blog.url}</p>
+        <p>likes {blog.likes}</p>
+        <p>{blog.user.name}</p>
+      </div>
+    </Togglable>
   </div>
 );
 
