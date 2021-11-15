028f91693e5ef870e44765bc28fb4b98b8fe4766
Krittanu Royapan <krittanu.riyapan@gmail.com>
2021-11-15 17:10:12 +0700

    Add inline style to blog component

--- src/components/Blog.js
+++ src/components/Blog.js
@@ -1,16 +1,27 @@
 import React from 'react';
 import Togglable from './Togglable';
-const Blog = ({ blog }) => (
-  <div>
-    {blog.title} {blog.author}
-    <Togglable showLabel="view" hideLabel="hide">
+const Blog = ({ blog }) => {
+  const blogStyle = {
+    paddingTop: 10,
+    paddingLeft: 2,
+    border: 'solid',
+    borderWidth: 1,
+    marginBottom: 5,
+  };
+  return (
+    <div style={blogStyle}>
       <div>
-        <p>{blog.url}</p>
-        <p>likes {blog.likes}</p>
-        <p>{blog.user.name}</p>
+        {blog.title} {blog.author}
+        <Togglable showLabel="view" hideLabel="hide">
+          <div>
+            <p>{blog.url}</p>
+            <p>likes {blog.likes}</p>
+            <p>{blog.user.name}</p>
+          </div>
+        </Togglable>
       </div>
-    </Togglable>
-  </div>
-);
+    </div>
+  );
+};
 
 export default Blog;
