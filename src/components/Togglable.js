0abc03a690b6cbdb481a03f1911227714b205692
Krittanu Royapan <krittanu.riyapan@gmail.com>
2021-11-15 17:05:02 +0700

    Add props for button name

--- src/components/Togglable.js
+++ src/components/Togglable.js
@@ -1,25 +1,27 @@
 import React, { useState, useImperativeHandle } from 'react';
 
-const Togglable = React.forwardRef(({ children, buttonLabel }, ref) => {
-  const [isVisible, setIsVisible] = useState(false);
-  const toggleVisibility = () => {
-    setIsVisible(!isVisible);
-  };
-  useImperativeHandle(ref, () => {
-    return { toggleVisibility };
-  });
-  return (
-    <div>
-      {isVisible ? (
-        <div>
-          {children}
-          <button onClick={toggleVisibility}>cancel</button>
-        </div>
-      ) : (
-        <button onClick={toggleVisibility}>{buttonLabel}</button>
-      )}
-    </div>
-  );
-});
+const Togglable = React.forwardRef(
+  ({ children, showLabel, hideLabel }, ref) => {
+    const [isVisible, setIsVisible] = useState(false);
+    const toggleVisibility = () => {
+      setIsVisible(!isVisible);
+    };
+    useImperativeHandle(ref, () => {
+      return { toggleVisibility };
+    });
+    return (
+      <div>
+        {isVisible ? (
+          <div>
+            {children}
+            <button onClick={toggleVisibility}>{hideLabel}</button>
+          </div>
+        ) : (
+          <button onClick={toggleVisibility}>{showLabel}</button>
+        )}
+      </div>
+    );
+  }
+);
 
 export default Togglable;
