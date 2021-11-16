6eb48a9f0540161b6582aff4e73b3db0080f8eca
Krittanu Royapan <krittanu.riyapan@gmail.com>
2021-11-16 17:44:43 +0700

    Create bloglist test

---
+++ cypress/integration/bloglists_app.spec.js
@@ -0,0 +1,13 @@
+describe('BlogLists app', function () {
+  beforeEach(function () {
+    cy.request('POST', 'http://localhost:3003/api/testing/reset');
+    cy.visit('http://localhost:3000');
+  });
+
+  it('Login form is shown', function () {
+    cy.contains('log in to application');
+    cy.contains('username');
+    cy.contains('password');
+    cy.contains('submit');
+  });
+});
