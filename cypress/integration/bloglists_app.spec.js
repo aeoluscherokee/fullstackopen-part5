0e22f596976267bb5c9f3542d15844316c236577
Krittanu Royapan <krittanu.riyapan@gmail.com>
2021-11-16 17:58:02 +0700

    Test login is successful

--- cypress/integration/bloglists_app.spec.js
+++ cypress/integration/bloglists_app.spec.js
@@ -10,4 +10,24 @@ describe('BlogLists app', function () {
     cy.contains('password');
     cy.contains('submit');
   });
+
+  describe('Login', function () {
+    it('succeeds with correct credentials', function () {
+      cy.get('#username').type('abswan');
+      cy.get('#password').type('nawsba');
+      cy.contains('submit').click();
+      cy.contains('logout');
+      cy.contains('create new blog');
+      cy.contains('Canonical string reduction Edsger W. Dijkstra');
+      cy.contains('First class tests Robert C. Martin');
+      cy.contains('React patterns Michael Chan');
+      cy.contains('Go To Statement Considered Harmful Edsger W. Dijkstra');
+      cy.contains('Type wars Robert C. Martin');
+      cy.contains('TDD harms architecture Robert C. Martin');
+    });
+
+    // it('fails with wrong credentials', function () {
+    //   // ...
+    // });
+  });
 });
