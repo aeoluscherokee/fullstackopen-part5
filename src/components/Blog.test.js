3a81fcd07baf45ca4ff733cd5f9c9d64e2b39949
Krittanu Royapan <krittanu.riyapan@gmail.com>
2021-11-16 14:47:57 +0700

    Test view button in Blog

--- src/components/Blog.test.js
+++ src/components/Blog.test.js
@@ -1,23 +1,23 @@
 import React from 'react';
 import '@testing-library/jest-dom/extend-expect';
-import { render } from '@testing-library/react';
+import { render, fireEvent } from '@testing-library/react';
 import Blog from './Blog';
 
-test('renders content', () => {
+test('renders title and author as default', () => {
   const blog = {
     id: '1',
-    title: 'Test',
-    url: 'Test',
-    author: 'Test',
-    likes: 'Test',
+    title: 'Wonderful Story',
+    url: 'http://wonderful.com/article/2536',
+    author: 'Adam Tyrell',
+    likes: 5,
     user: {
-      name: 'Test',
-      username: 'Test',
+      name: 'Steve McLarof',
+      username: 'steviemc',
     },
   };
   const userData = {
-    name: 'Test',
-    username: 'Test',
+    name: 'Steve McLarof',
+    username: 'steviemc',
     token: '',
   };
 
@@ -30,5 +30,46 @@ test('renders content', () => {
     />
   );
 
-  expect(component.container).toHaveTextContent('Test');
+  expect(component.container).toHaveTextContent('Wonderful Story Adam Tyrell');
+  expect(component.container).not.toHaveTextContent(
+    'http://wonderful.com/article/2536'
+  );
+  expect(component.container).not.toHaveTextContent('likes 5');
+});
+
+test('renders details after clicking view', () => {
+  const blog = {
+    id: '1',
+    title: 'Wonderful Story',
+    url: 'http://wonderful.com/article/2536',
+    author: 'Adam Tyrell',
+    likes: 5,
+    user: {
+      name: 'Steve McLarof',
+      username: 'steviemc',
+    },
+  };
+  const userData = {
+    name: 'Steve McLarof',
+    username: 'steviemc',
+    token: '',
+  };
+
+  const component = render(
+    <Blog
+      blog={blog}
+      updateLike={() => console.log('Test')}
+      deleteBlog={() => console.log('Test')}
+      user={userData}
+    />
+  );
+
+  const button = component.getByText('view');
+  fireEvent.click(button);
+
+  expect(component.container).toHaveTextContent('Wonderful Story Adam Tyrell');
+  expect(component.container).toHaveTextContent(
+    'http://wonderful.com/article/2536'
+  );
+  expect(component.container).toHaveTextContent('likes 5');
 });
