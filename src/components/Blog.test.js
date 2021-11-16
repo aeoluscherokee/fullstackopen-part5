20949ab464486ebdefc5dc285b178c9bbd69a82f
Krittanu Royapan <krittanu.riyapan@gmail.com>
2021-11-16 15:02:47 +0700

    Refactor Blog test code

--- src/components/Blog.test.js
+++ src/components/Blog.test.js
@@ -3,7 +3,7 @@ import '@testing-library/jest-dom/extend-expect';
 import { render, fireEvent } from '@testing-library/react';
 import Blog from './Blog';
 
-test('renders title and author as default', () => {
+describe('<Blog />', () => {
   const blog = {
     id: '1',
     title: 'Wonderful Story',
@@ -20,56 +20,41 @@ test('renders title and author as default', () => {
     username: 'steviemc',
     token: '',
   };
+  const mockHandler = jest.fn();
 
-  const component = render(
-    <Blog
-      blog={blog}
-      updateLike={() => console.log('Test')}
-      deleteBlog={() => console.log('Test')}
-      user={userData}
-    />
-  );
+  let component;
 
-  expect(component.container).toHaveTextContent('Wonderful Story Adam Tyrell');
-  expect(component.container).not.toHaveTextContent(
-    'http://wonderful.com/article/2536'
-  );
-  expect(component.container).not.toHaveTextContent('likes 5');
-});
+  beforeEach(() => {
+    component = render(
+      <Blog
+        blog={blog}
+        updateLike={mockHandler}
+        deleteBlog={mockHandler}
+        user={userData}
+      />
+    );
+  });
 
-test('renders details after clicking view', () => {
-  const blog = {
-    id: '1',
-    title: 'Wonderful Story',
-    url: 'http://wonderful.com/article/2536',
-    author: 'Adam Tyrell',
-    likes: 5,
-    user: {
-      name: 'Steve McLarof',
-      username: 'steviemc',
-    },
-  };
-  const userData = {
-    name: 'Steve McLarof',
-    username: 'steviemc',
-    token: '',
-  };
+  test('renders title and author as default', () => {
+    expect(component.container).toHaveTextContent(
+      'Wonderful Story Adam Tyrell'
+    );
+    expect(component.container).not.toHaveTextContent(
+      'http://wonderful.com/article/2536'
+    );
+    expect(component.container).not.toHaveTextContent('likes 5');
+  });
 
-  const component = render(
-    <Blog
-      blog={blog}
-      updateLike={() => console.log('Test')}
-      deleteBlog={() => console.log('Test')}
-      user={userData}
-    />
-  );
-
-  const button = component.getByText('view');
-  fireEvent.click(button);
-
-  expect(component.container).toHaveTextContent('Wonderful Story Adam Tyrell');
-  expect(component.container).toHaveTextContent(
-    'http://wonderful.com/article/2536'
-  );
-  expect(component.container).toHaveTextContent('likes 5');
+  test('renders details after clicking view', () => {
+    const button = component.getByText('view');
+    fireEvent.click(button);
+    expect(component.container).toHaveTextContent(
+      'Wonderful Story Adam Tyrell'
+    );
+    expect(component.container).toHaveTextContent(
+      'http://wonderful.com/article/2536'
+    );
+    expect(component.container).toHaveTextContent('likes 5');
+    expect(component.container).toHaveTextContent('hide');
+  });
 });
